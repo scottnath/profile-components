105d44b5c4fd30bd30f608006b9f1cf1aaddd711
Scott Nath <github@scottnath.com>
2023-09-08 09:45:48 -0400

    :rocket: releases setup

--- .releaserc.js
+++ .releaserc.js
@@ -1,9 +1,3 @@
 export default {
   extends: 'storydocker-utilities/releases',
-  branches: [
-    { name: 'main', channel: 'latest', prerelease: false },
-    { name: 'setup-module', channel: 'next', prerelease: true },
-  ],
-  debug: true,
-  tagFormat: 'profile-components@${version}',
 };
