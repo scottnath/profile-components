3d374b602b8abd2b5ef1688129c7e448e1ce7033
Scott Nath <github@scottnath.com>
2023-09-08 19:19:18 -0400

    :art: add container queries to repository.js

--- .storybook/viewports.js
+++ .storybook/viewports.js
@@ -3,17 +3,24 @@
 import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 export const customViewports = {
-  container200: {
-    name: 'Container 200px',
+  containerMin1: {
+    name: 'ContainerMin1: 200px',
     styles: {
       width: '200px',
       height: '100%',
     },
   },
-  container300: {
-    name: 'Container 300px',
+  containerMin2: {
+    name: 'ContainerMin2: 300px',
     styles: {
       width: '300px',
+      height: '100%',
+    },
+  },
+  containerMin3: {
+    name: 'ContainerMin3: 400px',
+    styles: {
+      width: '400px',
       height: '100%',
     },
   },
