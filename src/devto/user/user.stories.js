c7d767ecd09c98fa91425ae6c0dff536d6d3b5d5
Scott Nath <github@scottnath.com>
2023-10-18 10:08:01 -0400

    :art: clean up devto styles n stories

--- src/devto/user/user.stories.js
+++ src/devto/user/user.stories.js
@@ -4,22 +4,16 @@ import { parseFetchedUser } from './content';
 import { parseFetchedPost } from '../post/content';
 import { default as userScottnath } from '../fixtures/generated/user--scottnath.json';
 import { default as postDependabot } from '../fixtures/generated/post--dependabot.json';
+import { default as postBugfix } from '../fixtures/generated/post--bugfix-multi-vite.json';
 
 import './index.js';
-
-const stringify = (obj) => JSON.stringify(obj).replace(/"/g, "&quot;");
-
-const attrs = (args) => Object.entries(args)
-.filter(([key, value]) => value)
-.map(([key, value]) => `${key}="${value}"`)
-.join(' ');
 
 export default {
   title: 'DevTo/devto-user',
   component: 'devto-user',
   tags: ['autodocs'],
   render: (args) => {
-    const attributes = attrs(args);
+    const attributes = attrGen(args);
   
     return `
       <devto-user ${attributes}></devto-user>
@@ -31,7 +25,6 @@ export default {
 export const User = {
   args: {
     ...parseFetchedUser(userScottnath),
-    post_count: 123456,
   },
   // play: async ({ args, canvasElement, step }) => {
   //   const elements = await getElements(canvasElement);
@@ -43,7 +36,15 @@ export const UserPosts = {
   args: {
     ...User.args,
     latest_post: stringify(parseFetchedPost(postDependabot)),
+    popular_post: stringify(parseFetchedPost(postBugfix)),
   }
+}
+
+export const OnlyRequired = {
+  args: {
+    username: userScottnath.username,
+    name: userScottnath.name,
+  },
 }
 
 export const Fetch = {
@@ -69,28 +70,47 @@ export const FetchOverides = {
     profile_image: 'cat-square.jpeg',
     joined_at: 'Jan 1, 1979',
     post_count: 1000000,
-    latest_post: stringify({
+    popular_post: stringify({
       title: 'Meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow',
       url: 'http://example.com',
       cover_image: 'cat-1000-420.jpeg'
     }),
+    latest_post: stringify({
+      title: 'Mess? Make your human blame the dog',
+      url: 'http://example.com',
+      cover_image: 'cat-glasses-1000-420.jpeg'
+    }),
+  },
+}
+
+export const FetchError = {
+  args: {
+    username: 'not-a-real-user',
+    fetch: true,
+  },
+  parameters: {
+    mockData: [
+      generateMockResponse({username: 'not-a-real-user'}, 'users', 404),
+    ]
   },
 }
 
 export const ContainerCheck = {
   args: {
     ...FetchOverides.args,
-    theme: 'dark'
   },
 
   render: (args) => {
-    const attributes = attrs(args);
+    const attributes = attrGen(args);
   
     return `
-      <div style="display: flex; width: 1000px; margin: 1em;">
-        <devto-user ${attributes} style="flex: 1 1 200px;"></devto-user>
-        <devto-user ${attributes} style="flex: 1 1 300px;"></devto-user>
-        <devto-user ${attributes} style="flex: 1 1 400px;"></devto-user>
+      <div style="display: flex; flex-wrap: wrap; width: 1000px; margin: 1em;">
+        <devto-user ${attributes} theme="dark" style="flex: 0 0 200px;"></devto-user>
+        <devto-user ${attributes} theme="light" style="flex: 0 0 200px;"></devto-user>
+        <devto-user ${attributes} theme="dark" style="flex: 0 0 300px;"></devto-user>
+        <devto-user ${attributes} theme="light" style="flex: 0 0 300px;"></devto-user>
+        <devto-user ${attributes} theme="dark" style="flex: 0 0 400px;"></devto-user>
+        <devto-user ${attributes} theme="light" style="flex: 0 0 400px;"></devto-user>
       </div>
     `;
   }
